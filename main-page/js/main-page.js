bc75f65b14def23f71df61e5e3c927b5b3665282
Yan Karpinskyi <yankarp377@gmail.com>
2025-06-22 17:58:46 +0300

    finished main page

--- main-page/js/main-page.js
+++ main-page/js/main-page.js
@@ -1,5 +1,6 @@
 const categoriesBtn = document.querySelector('.categories-btn');
 const categoriesMenu = document.querySelector('.cat-menu');
+const favIcons = document.querySelectorAll('.fav-icon-card')
 
 categoriesBtn.addEventListener('click', () => {
     if (categoriesMenu.style.display =='block') {
@@ -8,3 +9,22 @@ categoriesBtn.addEventListener('click', () => {
         categoriesMenu.style.display = 'block';
     }
 })
+
+document.querySelectorAll('.grade').forEach(p => {
+    const text = p.textContent.replace(/[^\d.,]/g, '').replace(',', '.');
+    const number = parseFloat(text);
+
+    if (!isNaN(number) && Number.isInteger(number)) {
+        p.style.marginLeft = '0.5vw';
+    }
+});
+
+favIcons.forEach(favIcon => {
+    favIcon.addEventListener('click', () => {
+        if (favIcon.src.endsWith('fav-icon-default.png')) {
+            favIcon.src = './img/fav-icon.png';
+        } else {
+            favIcon.src = './img/fav-icon-default.png';
+        }
+    });
+});
